5e39fd670b0d1a68741db610cdcbdd647295e4f5
Carolina <carolina.cortes@fing.edu.uy>
2025-11-07 13:56:55 +0100

    a

--- sql/classification/sign_precision_changes.sql
+++ sql/classification/sign_precision_changes.sql
@@ -1,8 +1,5 @@
 CREATE EXTENSION IF NOT EXISTS unaccent;
 
-CREATE INDEX IF NOT EXISTS idx_change_metadata_lookup 
-  ON :change_metadata (revision_id, property_id, value_id, change_target, change_metadata); --  for WHERE filter
-
 ALTER TABLE :change
 ADD COLUMN IF NOT EXISTS precision_change BOOLEAN DEFAULT FALSE;
 
@@ -19,7 +16,8 @@ c.target = 'PROPERTY_VALUE' AND
         -- precision changes, the last part has a levenshtein distance that is small (<= 3)
         c.datatype IN ('quantity') AND
         -- they both have precision
-        REGEXP_REPLACE(new_value->>0, '[,]', '.', 'g') ~ '[.]' AND REGEXP_REPLACE(old_value->>0, '[,]', '.', 'g') ~ '[.]'
+        REGEXP_REPLACE(new_value->>0, '[,]', '.', 'g') ~ '[.]' AND 
+        REGEXP_REPLACE(old_value->>0, '[,]', '.', 'g') ~ '[.]' AND
         levenshtein(SPLIT_PART(REGEXP_REPLACE(new_value->>0, '[,]', '.', 'g'), '.', 2), SPLIT_PART(REGEXP_REPLACE(old_value->>0, '[,]', '.', 'g'), '.', 2)) <= 3
     )
     OR 
@@ -27,12 +25,14 @@ c.target = 'PROPERTY_VALUE' AND
         c.datatype IN ('globecoordinate') AND 
         (
             (
-                REGEXP_REPLACE(new_value::latitude, '[,]', '.', 'g') ~ '[.]' AND REGEXP_REPLACE(old_value::latitude, '[,]', '.', 'g') ~ '[.]'
+                REGEXP_REPLACE(new_value::latitude, '[,]', '.', 'g') ~ '[.]' AND 
+                REGEXP_REPLACE(old_value::latitude, '[,]', '.', 'g') ~ '[.]' AND
                 levenshtein(SPLIT_PART(REGEXP_REPLACE(new_value::latitude, '[,]', '.', 'g'), '.', 2), SPLIT_PART(REGEXP_REPLACE(old_value::latitude, '[,]', '.', 'g'), '.', 2)) <= 3
             )
             OR
             (
-                REGEXP_REPLACE(new_value::longitude, '[,]', '.', 'g') ~ '[.]' AND REGEXP_REPLACE(old_value::longitude, '[,]', '.', 'g') ~ '[.]'
+                REGEXP_REPLACE(new_value::longitude, '[,]', '.', 'g') ~ '[.]' AND 
+                REGEXP_REPLACE(old_value::longitude, '[,]', '.', 'g') ~ '[.]' AND
                 levenshtein(SPLIT_PART(REGEXP_REPLACE(new_value::longitude, '[,]', '.', 'g'), '.', 2), SPLIT_PART(REGEXP_REPLACE(old_value::longitude, '[,]', '.', 'g'), '.', 2)) <= 3
             )
         )
@@ -40,11 +40,14 @@ c.target = 'PROPERTY_VALUE' AND
 )
 ; 
 
+ALTER TABLE :change
+ADD COLUMN IF NOT EXISTS sign_change BOOLEAN DEFAULT FALSE;
+
 UPDATE :change 
 SET sign_change = TRUE
 WHERE
 NOT (
-formatting OR typo OR value_refinement OR value_unrefinement OR
+formatting OR typo OR value_refinement OR value_unrefinement OR precision_change OR
 reverted_edit OR reversion) AND
 c.action = 'UPDATE' AND 
 c.target = 'PROPERTY_VALUE' AND
